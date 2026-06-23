cf2f9d78dd24a7209544d2a9c1d80e1201edd24e
Arrel Neumiller <1020771+rlneumiller@users.noreply.github.com>
2026-06-23 22:21:05 +0100

    Update container id comfyui-v0.25.0:commit-e16c3d25-gguf

---
+++ neu-build-image.sh
@@ -0,0 +1,1 @@
+podman build -t comfyui-v0.25.0:commit-e16c3d25-gguf .
--- neu-run-container.sh
+++ neu-run-container.sh
@@ -12,4 +12,4 @@ podman run -it --rm --name comfyui \
  -v ~/comfyui/custom_nodes:/workspace/ComfyUI/custom_nodes:Z \
  -v ~/comfyui/settings:/workspace/ComfyUI/user:Z \
  -v ~/comfyui/temp:/workspace/ComfyUI/temp:Z \
- localhost/comfyui-v0.24:latest
+ localhost/comfyui-v0.25.0:commit-e16c3d25-gguf
